d88cd645482f669d65cd88249c08bb221cd66423
joaquincruzat <joaquincruzat@gmail.com>
2022-03-12 14:04:26 -0300

    ejercicio finalizado

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,22 @@
-let num = parseInt (prompt("Por favor, ingrese un número"));
+let num = parseInt(prompt("Por favor, ingrese un número"));
+let factorial = 1;
 
- num => (num < 1 || num > 20);
-   console.error ("El número ingresado está fuera de rango"); 
+let tablaDeMultiplicar = num => {if (num < 1 || num > 20) {
+        console.error("El número ingresado está fuera de rango")
+    } else {
+        for (multiplicador = 1; multiplicador <= num; multiplicador++) {
+            console.log(multiplicador + " x " + num + " = " + multiplicador * num);
+        } for (let i = 1; i <=num; i++) {
+            factorial = factorial*i;
+
+        }
+        console.log ("El factorial de "+num+" es: "+factorial);
+    }
+}
+
+console.log (tablaDeMultiplicar(num));
+
+
+
+
+
